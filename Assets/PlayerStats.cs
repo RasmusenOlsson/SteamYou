53bb051321ca3f778309438c4e1560bcfe853484
WWalter <walter.wanerskog2@gmail.com>
2024-02-12 14:44:36 +0100

    Mid making the map collider work

--- Assets/PlayerStats.cs
+++ Assets/PlayerStats.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    [SerializeField] int HumanPose = 3;
+    [SerializeField] int HP = 3;
     [SerializeField] int Swords = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void TakeDamage()
+    void TakeDamage(int damage)
     {
+        HP -= damage;
+        if (HP <= 0)
+            Die();
+    }
 
+    public void Die()
+    {
+        Debug.Log("You Died");
+        Destroy(gameObject); // Destroy the GameObject to which this script is attached
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Map")) // Assuming you've tagged the map boundary collider appropriately
+        {
+            // Call Die function from PlayerStats script
+            Die();
+        }
     }
 }
