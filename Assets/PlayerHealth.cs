fa8dbdf366d0a527550ccea21ff06b29b2b5f5dc
RasmusenOlsson <142796466+RasmusenOlsson@users.noreply.github.com>
2024-02-12 15:44:03 +0100

    Added UI

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 3;
+    public int currenthealth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currenthealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(1);
+        }
+    }
+    // allting är inte fullt klar än om någon märker lol
+    void TakeDamage(int damage)
+    {
+        currenthealth -= damage;
+    }
+}
